96685ea51aeffbd9643eb10875a0fa36d919e568
nakamurakazuki <nakamura.kazuki@algoage.co.jp>
2023-11-23 15:30:08 +0900

    node-sqlite3 をコールバックの形式で使い、エラーなし、エラーありのプログラムを作成した

---
+++ 03.asynchronous/callback.js
@@ -0,0 +1,72 @@
+import sqlite3 from "sqlite3";
+import timers from "timers";
+
+const db1 = new sqlite3.Database(":memory:");
+
+db1.run(
+  "CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  function () {
+    db1.run(
+      "INSERT INTO book (title) VALUES (?)",
+      ["Sample Title"],
+      function () {
+        console.log("Inserted record ID:", this.lastID);
+
+        db1.get(
+          "SELECT * FROM book WHERE id = ?",
+          [this.lastID],
+          function (err, row) {
+            if (err) {
+              console.error("Error retrieving record:", err.message);
+            } else {
+              console.log("Retrieved record:", row);
+            }
+
+            timers.setTimeout(function () {
+              db1.run("DROP TABLE IF EXISTS book", function () {
+                db1.close();
+
+                runErrorProgram();
+              });
+            }, 100);
+          }
+        );
+      }
+    );
+  }
+);
+
+function runErrorProgram() {
+  const db2 = new sqlite3.Database(":memory:");
+
+  db2.run(
+    "CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+    function () {
+      db2.run(
+        "INSERT INTO memo (title) VALUES (?)",
+        ["Sample Title"],
+        function (err) {
+          if (err) {
+            console.error("Error inserting record:", err.message);
+          }
+
+          db2.get(
+            "SELECT * FROM memo WHERE id = ?",
+            [999],
+            function (err, row) {
+              if (err) {
+                console.error("Error retrieving record:", err.message);
+              } else {
+                console.log("Retrieved record:", row);
+              }
+
+              db2.run("DROP TABLE IF EXISTS book", function () {
+                db2.close();
+              });
+            }
+          );
+        }
+      );
+    }
+  );
+}
